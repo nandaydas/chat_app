38383dee2c96b03f744d4ad5a982190cf36e35d7
Dipan Nama <dipannama91@gmail.com>
2025-10-01 10:30:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'routes/app_routes.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
